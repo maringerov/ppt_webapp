09a2ba9a64afc3bec93cbc938a1d14c9f547c1b5
Marin Gerov <marin.gerov@gmail.com>
2014-04-22 00:06:48 +0200

    Adding all the files

---
+++ app/js/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/* App Module */
+
+var phonecatApp = angular.module('phonecatApp', [
+  'ngRoute',
+  'phonecatControllers',
+  'phonecatFilters',
+  'phonecatServices'
+]);
+
+phonecatApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/phones', {
+        templateUrl: 'partials/phone-home.html',
+        controller: 'PhoneListCtrl'
+      }).
+      when('/phones/:phoneId', {
+        templateUrl: 'partials/phone-detail.html',
+        controller: 'PhoneDetailCtrl'
+      }).
+      otherwise({
+        redirectTo: '/phones'
+      });
+  }]);
+
+phonecatApp.filter("emptyifblank", function(){ return function(object, query){
+    if(!query)
+        return {}
+    else
+        return object;
+}});
